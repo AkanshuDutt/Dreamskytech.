4fcdd08215baa640a0ff0777d4c588717f424d08
Akanshu_dutt <duttakanshu@gmail.com>
2025-08-29 17:36:28 +0530

    cooking

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,4 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  env: {
-    DATABASE_URL: process.env.DATABASE_URL,
-  },
-  /* config options here */
+const nextConfig = {
+  output: "export",
 };
-
 export default nextConfig;
